8963462ddfbac5e9e3ca9792a4c3f157f0a4f8ac
Sania-Shk <saniashaikhhh@example.com>
2025-03-13 08:04:06 +0530

    Saved all pending changes before pulling

--- Backend(Flask ApI)/config.py
+++
@@ -1,7 +0,0 @@
-# This file will store configurations like database URL and secret keys.
-
-
-class Config:
-    SECRET_KEY = "your_secret_key_here"
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:sql-404@localhost/business_data_automation"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
